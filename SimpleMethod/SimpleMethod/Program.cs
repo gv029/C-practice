0e1b2e7702e172e7d45b352f0209be3a69bf4201
Gabriel Vidal <gxv110130@gmail.com>
2018-05-10 19:01:45 -0500

    Add files via upload

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = NameChecker();
+            string y = ReverseString(x);
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            string reverseMessage = String.Concat(messageArray);
+
+            return reverseMessage;
+        }
+
+        private static string NameChecker()
+        {
+            string firstName;
+            string lastName;
+            string city;
+
+            Console.WriteLine("Enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Which city do you live in? ");
+            city = Console.ReadLine();
+
+            string message = firstName + " " + lastName + " " + city;
+
+            Console.WriteLine(message);
+            return message;
+        }
+        
+        
+    }
+ 
+
+}
---
+++ stringManipulation/stringManipulation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace stringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myString = "My \"so-called\" life";
+            //string myString = "What if I need a \nnew line?";
+            //string myString = "Go do your c:\\Drive";
+            //string myString = @"Go to your c:\Drive";
+
+            //string myString = String.Format("{0} = {1}", "First", "Second");
+
+            //string myString = string.Format("{0:C}", 123);
+
+            //string myString = string.Format("{0:N}", 1234567890);
+
+            //string myString = string.Format("{0:P}", .123);
+
+            //custom formatting
+            //string myString = string.Format("Phone number: {0:(###)-###-####}", 2146687428);
+
+
+
+
+
+            //string myString = " a somewhat long sentence that doesn't really say anything ";
+
+            // myString = myString.Substring(1,myString.Length-2);
+            // myString = myString.ToUpper();
+            //myString = myString.Replace(" ", "*");
+            //myString = myString.Remove(12);
+
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+
+            }
+
+            Console.WriteLine(myString.Length);
+
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
